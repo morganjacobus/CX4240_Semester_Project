60197c96d08c63ba5e4728b86d22e5636a60ae7b
sakofeye <36825178+sakofeye@users.noreply.github.com>
2018-02-25 20:35:31 -0500

    Update pre_process.py

--- pre_process.py
+++ pre_process.py
@@ -4,6 +4,7 @@ Pre-Processing Python Script
 """
 import pandas as pd
 import random as rand
+from sklearn.feature_extraction.text import CountVectorizer
 
 filename = "C:\Users\morga\Documents\Georgia Tech\Classes\CX 4240\Data\Train.csv"
 
@@ -11,3 +12,7 @@ n = sum(1 for line in open(filename)) - 1
 s = 1000
 skip = sorted(rand.sample(xrange(1,n+1),n-s))
 df = pd.read_csv(filename, skiprows = skip)
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(df['Body'])
+X_train_counts.shape
+count_vect.vocabulary_.get(u'algorithm')
