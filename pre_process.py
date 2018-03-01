d5fc12c031b9f9c9968dfc295c85835caf5fc2c2
sakofeye <36825178+sakofeye@users.noreply.github.com>
2018-03-01 14:45:12 -0500

    Update pre_process.py

--- pre_process.py
+++ pre_process.py
@@ -5,16 +5,13 @@ Pre-Processing Python Script
 import pandas as pd
 import random as rand
 from sklearn.feature_extraction.text import CountVectorizer
-
-filename = "C:\Users\morga\Documents\Georgia Tech\Classes\CX 4240\Data\Train.csv"
-
-n = sum(1 for line in open(filename)) - 1
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+filename = 'Test.csv'
+n = sum(1 for line in open(filename))
 s = 1000
 skip = sorted(rand.sample(xrange(1,n+1),n-s))
-df = pd.read_csv(filename, skiprows = skip)
-count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(df['Body'])
-X_train_counts.shape
-count_vect.vocabulary_.get(u'algorithm')
-tf_transformer = TfidfTransformer()
-X_train_tfidf = tf_transformer.fit_transform(X_train_counts)
+df = pd.read_csv(filename,skiprows=skip)
+txt_clf = Pipeline([('vect', CountVectorizer()),('tfidf', TfidfTransformer()),
+                    ('RF', RandomForestClassifier())])
