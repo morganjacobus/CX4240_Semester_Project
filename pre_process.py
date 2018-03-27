d9ba27fbb6b250301bce21cd2abf82a5ff1e4454
morganjacobus <31784328+morganjacobus@users.noreply.github.com>
2018-03-27 11:31:29 -0400

    Creates X_train and y_train

--- pre_process.py
+++ pre_process.py
@@ -3,13 +3,38 @@
 Pre-Processing Python Script
 """
 import pandas as pd
-import random as rand
-
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
+from scipy import sparse
 filename = "C:\Users\morga\Documents\Georgia Tech\Classes\CX 4240\Data\Train.csv"
 
-n = sum(1 for line in open(filename)) - 1
-s = 1000
-skip = sorted(rand.sample(xrange(1,n+1),n-s))
-df = pd.read_csv(filename, skiprows = skip)
 
-body = df["Body"]
+chunks = pd.read_csv(filename,chunksize = 1000,iterator = True,index_col = 0)
+
+subset = chunks.get_chunk(1001)
+
+subset["Text"] = subset["Title"] + " " + subset["Body"]
+
+labels = []
+
+for item in subset["Tags"]:
+    labels.append(item.split())
+
+subset["labels"] = labels
+y_train = MultiLabelBinarizer().fit_transform(subset["labels"])
+
+
+vectorizer = CountVectorizer()
+
+vectorizer.fit(subset["Text"])
+
+print(vectorizer.vocabulary_)
+
+X_train = []
+
+vector = vectorizer.transform(subset["Text"])
+
+X_train = vector.toarray()
+
